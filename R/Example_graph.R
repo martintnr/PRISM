182f13424909ba6728948e58c5a48913ebf6ebd4
martin <tournaire.martin@gmail.com>
2024-03-14 12:33:55 +0100

    graph

--- R/Example_graph.R
+++ R/Example_graph.R
@@ -17,7 +17,7 @@ Example_graph <- function(ListofTraits, Trait, TreshSelectionPvalues = 5e-08/len
 
   Somme$PvalPleioVar[Somme$PvalPleioVar < 1e-200] <- 1e-200 #Pour éviter des graphes bizarres
 
-  Somme$PvalGWAS <- 2*pnorm(q=abs(X$Zscore), lower.tail=FALSE)
+  Somme$PvalGWAS <- 2*pnorm(q=abs(X$Zscore/0.005), lower.tail=FALSE)
   message("Pvalues from GWAS calculated from from ZScores")
   Somme$PvalGWAS[Somme$PvalGWAS < 1e-200] <- 1e-200
 
@@ -25,7 +25,7 @@ Example_graph <- function(ListofTraits, Trait, TreshSelectionPvalues = 5e-08/len
   Somme$SynthPleio[ Somme$SynthPleio == "No supplementary info"] <- "\n Direct Effect \n "
   Somme$SynthPleio[ Somme$SynthPleio == "Detected Network Pleiotropy"] <- "\n Detected Network \n Pleiotropy \n "
   Somme$SynthPleio[ Somme$SynthPleio == "Suspected Vertical Pleiotropy"] <- "\n Suspected Vertical \n Pleiotropy \n "
-  Somme$SynthPleio[ Somme$SynthPleio == "\n Direct Effect \n " & Somme$PvalPleioVar < TreshSelectionPvalues ] <- "\n No Effect \n "
+  Somme$SynthPleio[ Somme$SynthPleio == "\n Direct Effect \n " & Somme$PvalPleioVar > TreshSelectionPvalues ] <- "\n No Effect \n "
 
 
 
@@ -34,8 +34,8 @@ Example_graph <- function(ListofTraits, Trait, TreshSelectionPvalues = 5e-08/len
         , xlab = "-log10(GWAS association p-value)", ylab = "-log10(PleioVar p-value)") +
     scale_color_manual(values = c("\n Detected Network \n Pleiotropy \n " = "#a23c33",
                                   "\n Direct Effect \n "="#45709d",
-                                  "\n Suspected Vertical \n Pleiotropy \n "="#82992a"),
-                                  "\n No Effect \n " = "#D3D3D3") +
+                                  "\n Suspected Vertical \n Pleiotropy \n "="#82992a",
+                                  "\n No Effect \n " = "#D3D3D3")) +
     labs(color = "Pleiotropy") +
     theme(text = element_text(size=20))
 
