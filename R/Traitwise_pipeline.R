efc30f4050e75f3fb24b6758fd0c95b670d217f4
martin <tournaire.martin@gmail.com>
2024-03-14 13:12:06 +0100

    messages

--- R/Traitwise_pipeline.R
+++ R/Traitwise_pipeline.R
@@ -22,7 +22,7 @@ Traitwise_pipeline <- function(ListofTraits, ParametersTable, Index ,NbCores, gz
 
     # Synthese
     TRAIT <- X
-    print(paste0("processing ", TRAIT))
+    print(paste0(TRAIT, "synthesis"))
 
     gc()
     TableBaseOEffect <- as_tibble(TableBase)
